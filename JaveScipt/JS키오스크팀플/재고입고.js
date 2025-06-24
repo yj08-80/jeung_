caded4f1a44599f46b4cfee900b9b485625aec21
yj08-80 <ys423423@gmail.com>
2025-06-24 15:47:11 +0900

    팀플4 수정

--- JaveScipt/JS키오스크팀플/재고입고.js
+++ JaveScipt/JS키오스크팀플/재고입고.js
@@ -10,15 +10,19 @@ let no = 3;
 // 입력 필드: 재고ID(자동), 제품 ID, 변동 유형 ('입고/출고') , 변동 수량, 입출사유 ,  변동날짜(오늘날짜 자동).
 // 동작:  성공/실패 메시지
 function stockIn(){
-    console.log( "stockIn exe")
+    console.log( "stockIn exe" )
     let idInput = document.querySelector("#idInput").value;
     console.log( idInput );
-    for( let i = 0 ; i <= productList.length-1 ; i++ ){
-        let product = productList[i];
-        if( idInput != product.id ){
-            alert("제품 아이디가 존재하지 않습니다");
-            return;
+    let found = false;
+    for (let i = 0; i < productList.length; i++) {
+        if (productList[i].pcode == idInput) {
+            found = true;
+            break;
         }
+    }
+    if (!found) {
+        alert("제품 아이디가 존재하지 않습니다");
+        return;
     }
     let select = document.querySelector("#select2").value;
     console.log( select );
@@ -35,15 +39,55 @@ function stockIn(){
     console.log( date );
     if( idInput == "" || select == "" || number == "" ||  textarea == "" ){
         alert("빈 항목이 있습니다.")
+        return;
     }
     const obj = {
         no : ++no,
         id : idInput,
         cname : select,
         number : number,
-        etc : textarea
+        etc : textarea,
+        변동날짜 : date
     }
     console.log( obj );
     productArray.push( obj );
     alert( "재고 입고 성공" );
+    console.log( productArray );
+    ViewAllInventoryLogs();
 }
+
+
+
+function ViewAllInventoryLogs(){ //console.log('----productcontent---')
+    const tbody=document.querySelector('.ttbody'); //console.log(tbody);
+    html='';
+    for(i=0; i<=productArray.length-1; i++){
+        let productA=productArray[i];
+        html+=  `<tr>
+                    <td>${productA.id}</td>
+                    <td>${productA.cname}.cno}</td>
+                    <td>${productA.number}</td>
+                    <td>${productA.etc.toLocaleString()}</td>
+                    <td>${productA.date}</td>
+                    <td><button onclick="ModifyInventoryHistory(${productA.no})" class="btnEdit">삭제</button></td>
+                 </tr>`
+    }                                                                       
+   
+    tbody.innerHTML=html; console.log(html);
+}
+
+
+
+// 입출사유 수정 함수
+
+function modifyReason( no ){ console.log( '>>modifyReason exe'); console.log( no ) ;
+    for( let i=0; i<=productArray.length-1; i++){
+        if( productArray[i].no == no ){
+            const reason = prompt('수정 할 입출사유: '); // 수정할 값 입력
+            productArray[i].no = reason;                // 입력값으로 수정
+            alert('[성공] 입출사유 수정'); 
+               // <--새로고침
+            return;
+        } // if end
+       } // for end
+    } // func end
