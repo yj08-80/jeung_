e5930c22de7a8b110b076add5ac2764fda2035d0
yj08-80 <ys423423@gmail.com>
2025-06-24 14:31:20 +0900

    팀플 js

---
+++ JaveScipt/JS키오스크팀플/재고입고.js
@@ -0,0 +1,6 @@
+const productArray = [
+    { no : 1 , id : 10, cname : "입고" , number : 3 , etc : asad },
+    { no : 2 , id : 20, cname : "출고" , number : 7 , etc : asda },
+    { no : 3 , id : 30, cname : "입고" , number : 2 , etc : dsad }
+]
+const categoryList = [ { no : 1 , cname : '입고' } , { no : 2 , cname : '출고' } ];
