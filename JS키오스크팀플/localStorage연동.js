d7dbd151f172409a3d95cb3b8b0901595f75dc9e
{name} <{email}>
2025-06-25 09:37:34 +0900

    팀플 5까지 수정

--- JS키오스크팀플/localStorage연동.js
+++ JS키오스크팀플/localStorage연동.js
@@ -1,10 +1,10 @@
 
-  let product = localStorage.getItem('productList'); //productList 이름의 속성값 가져오기
+//   let product = localStorage.getItem('productList'); //productList 이름의 속성값 가져오기
 
-localStorage.setItem('product', JSON.stringify(productList)); //로컬 스토리지에 저장
+// localStorage.setItem('product', JSON.stringify(productList)); //로컬 스토리지에 저장
 
-// localStorage 불러오기 함수 구현
-// localStorage 에서 제품 데이터를 불러와 products / inventoryLogs  배열에 파싱.
+// // localStorage 불러오기 함수 구현
+// // localStorage 에서 제품 데이터를 불러와 products / inventoryLogs  배열에 파싱.
 // let products = [];
 // let inventoryLogs = [];
 // function getLocalStorage() {
@@ -40,3 +40,17 @@ localStorage.setItem('product', JSON.stringify(productList)); //로컬 스토리
 //         return productList; // 웹스토리지에서 조회된 배열을 반환하는 함수 , 없으면 []빈배열 , 있으면 [{},{}]
 //     }
 // }
+
+function getLocalProduct(){
+    let productList=localStorage.getItem('productList');
+    if(productList==null){
+        productList=[];
+    }else{
+        productList=JSON.parse(productList);
+    }
+    return productList;
+
+}
+function setLocalProduct(productList){
+    localStorage.setItem("productList",JSON.stringify(productList));
+}
--- JS키오스크팀플/전체제품조회.js
+++ JS키오스크팀플/전체제품조회.js
@@ -3,14 +3,29 @@
 
 //배열
 
-const productList = [
-    { pcode : 1 , pname : "아메리카노" , pprice : 1500 , pcontent : "뉴크롭원두와 스페셜티 커피 원두를 블랜딩한 빽다방 아메리카노" , pimg :"https://placehold.co/100x100" },
-    { pcode : 2 , pname : "바닐라라떼" , pprice : 3000 , pcontent : "바닐라시럽" , pimg : "https://placehold.co/100x100" },
-    { pcode : 3 , pname : "헤이즐넛라떼" , pprice : 3500, pcontent : "헤이즐넛시럽", pimg : "https://placehold.co/100x100" }
-]
-let pcode = 3;
-//상품코드 : pcode , 상품명 : pname , 가격 : pprice , 간단한설명 : pcontent , 이미지 : pimg
+// const productList = [
+//     { pcode : 1 , pname : "아메리카노" , pprice : 1500 , pcontent : "뉴크롭원두와 스페셜티 커피 원두를 블랜딩한 빽다방 아메리카노" , pimg :"https://placehold.co/100x100" },
+//     { pcode : 2 , pname : "바닐라라떼" , pprice : 3000 , pcontent : "바닐라시럽" , pimg : "https://placehold.co/100x100" },
+//     { pcode : 3 , pname : "헤이즐넛라떼" , pprice : 3500, pcontent : "헤이즐넛시럽", pimg : "https://placehold.co/100x100" }
+// ]
 
+// let pcode = 3;
+//상품코드 : pcode , 상품명 : pname , 가격 : pprice , 간단한설명 : pcontent , 이미지 : pimg
+// ===========================================================local 수정자리
+// function getLocalProduct(){
+//     let productList=localStorage.getItem('productList');
+//     if(productList==null){
+//         productList=[];
+//     }else{
+//         productList=JSON.parse(productList);
+//     }
+//     return productList;
+
+// }
+// function setLocalProduct(productList){
+//     localStorage.setItem("productList",JSON.stringify(productList));
+// }
+// ===========================================================local 수정자리
 
 // 1. 제품 등록 함수 : 실행조건 : 등록버튼 onclick 클릭했을때
 
@@ -27,6 +42,7 @@ function productAdd(){ console.log( '>> productAdd exe');
     const pprice = ppriceInput.value;                   console.log(pprice);
     const pcontent = pcontentInput.value;               console.log(pcontent);
     // 첨부파일은 value 대신 files[0] ; 선택된 첨부파일의 첫번째 파일객체 가져오기
+
     const pimg = pimgInput.files[0];                    console.log(pimg);
     // 유효성 검사
         // 입력한 값이 없으면 등록 실패
@@ -35,8 +51,12 @@ function productAdd(){ console.log( '>> productAdd exe');
             return ; 
         } // if end
     //(3) 여러 데이터 객체로 구성하기.
+
+    let productList=getLocalProduct(); // ===========================================================local 수정자리
+        let pcode=productList.length == 0 ? 1 : productList[productList.length-1].pcode+1 // ===========================================================local 수정자리
+
     const obj = {
-        pcode : ++pcode,
+        pcode : pcode,
         pname : pname,
         pprice : Number(pprice),
         pcontent : pcontent,
@@ -45,7 +65,7 @@ function productAdd(){ console.log( '>> productAdd exe');
     };                                        console.log( obj);
     // (4) 구성한 객체를 배열에 저장
     productList.push( obj );                  console.log( productList); 
-    
+    setLocalProduct(productList); // ===========================================================local 수정자리
     // .value='';
     pnameInput.value='';
     ppriceInput.value='';
@@ -62,6 +82,7 @@ function viewAllProduct(){
     const tbody = document.querySelector(".tbody");
     console.log( tbody );
     let html = "";
+    let productList=getLocalProduct(); // ===========================================================local 수정자리
     for( let i = 0 ; i <= productList.length-1 ; i++){
         const product = productList[ i ];
         console.log( product );
@@ -79,10 +100,12 @@ function viewAllProduct(){
 }
 viewAllProduct();
 function DeleteProduct(pcode){ console.log('---exe----')
+    let productList=getLocalProduct(); // ===========================================================local 수정자리
     for(i=0; i<=productList.length-1 ; i++){
         let product=productList[i]; console.log(product);
         if(product.pcode==pcode){
             productList.splice(i,1);
+            setLocalProduct(productList); // ===========================================================local 수정자리
             alert("제품 삭제 완료");
             viewAllProduct();
             return;
